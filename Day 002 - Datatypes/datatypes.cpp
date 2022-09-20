a18079ec16ec8489bbfde36efaed36168000d248
Kinjal Kanjiya <77874244+KinjalKanjiya@users.noreply.github.com>
2022-09-20 17:59:52 +0530

    Update datatypes.cpp

--- Day 002 - Datatypes/datatypes.cpp
+++ Day 002 - Datatypes/datatypes.cpp
@@ -1,1 +1,25 @@
+#include<iostream>
+using namespace std;
+int main(){
+
+    int a;
+    cout<<"size of int "<<sizeof(a)<<endl;
+
+    float b;
+    cout<<"size of float "<<sizeof(b)<<endl;
+
+    char c;
+    cout<<"size of char "<<sizeof(c)<<endl;
+
+    bool d;
+    cout<<"size of bool "<<sizeof(d)<<endl;
+
+    short int s;
+    cout<<"size of shortint "<<sizeof(s)<<endl;
+
+    long int l;
+    cout<<"size of longint "<<sizeof(l)<<endl;
+
+    return 0;
+}
 
