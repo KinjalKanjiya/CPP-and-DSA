3154f647fc38f6f0e5f1f901508554f68e0e0da9
Kinjal Kanjiya <77874244+KinjalKanjiya@users.noreply.github.com>
2022-09-21 18:30:06 +0530

    Code added

--- Day 003 - InputOutput/SumOfTwoNumbers.cpp
+++ Day 003 - InputOutput/SumOfTwoNumbers.cpp
@@ -1,1 +1,13 @@
-
+//Program to read two numbers and print sum of the two numbers. 
+#include<iostream>
+using namespace std;
+int main(){
+    int num1;
+    cin>>num1;
+    int num2;
+    cin>>num2;
+    int sum;
+    sum = num1 + num2;
+    cout<<"Sum is : "<<sum;
+    return 0;
+}
