eb7fef0ec4ac9eb0b141c51c170d5bb13f7454f0
Kinjal Kanjiya <77874244+KinjalKanjiya@users.noreply.github.com>
2022-09-22 18:44:51 +0530

    Code added

--- Day 004 - Ifelse/OddEven.cpp
+++ Day 004 - Ifelse/OddEven.cpp
@@ -1,1 +1,16 @@
+// program to print number is even or odd
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cin>>n;
+    if(n%2==0){
+        cout<<n<<" is even"<<endl;
+    }
+    else{
+        cout<<n<<" is odd"<<endl;
+    }
+    return 0;
+}
 
