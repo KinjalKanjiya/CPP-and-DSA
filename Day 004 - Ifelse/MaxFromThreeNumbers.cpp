07a482ff05d1ef0fda7ca75cf9d986c0959b73be
Kinjal Kanjiya <77874244+KinjalKanjiya@users.noreply.github.com>
2022-09-22 18:49:53 +0530

    Code added

--- Day 004 - Ifelse/MaxFromThreeNumbers.cpp
+++ Day 004 - Ifelse/MaxFromThreeNumbers.cpp
@@ -1,1 +1,29 @@
+//program to find maximum from 3 numbers
+#include<iostream>
+using namespace std;
+int main(){
 
+    int num1,num2,num3;
+    cin>>num1>>num2>>num3;
+    if(num1>num2){
+        if(num1>num3){
+            cout<<num1<<" is maximum number";
+        }
+        else{
+            
+            cout<<num3<<" is maximum number";
+        }
+    }
+    else{
+        if(num2>num3){
+            cout<<num2<<" is maximum number";
+        }
+        else
+        {
+            cout<<num3<<" is maximum number";
+        }
+    }
+   
+    
+    return 0;
+}
