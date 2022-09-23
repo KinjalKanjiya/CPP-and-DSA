44345ad91eca8816dcd9d6e875ba1b55a3396ed9
Kinjal Kanjiya <77874244+KinjalKanjiya@users.noreply.github.com>
2022-09-23 18:43:12 +0530

    Code added

--- Day 005 - Loops/dowhileLoop.cpp
+++ Day 005 - Loops/dowhileLoop.cpp
@@ -1,1 +1,14 @@
+//Program using dowhile loop.
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    do{
+        cout<<"n"<<endl;
+        cin>>n;
 
+    }
+    while(n>0);
+    return 0;
+}
