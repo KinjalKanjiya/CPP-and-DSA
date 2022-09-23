96fc269d995510aac91640a4b948db0cf012bb89
Kinjal Kanjiya <77874244+KinjalKanjiya@users.noreply.github.com>
2022-09-23 18:40:54 +0530

    Code added

--- Day 005 - Loops/WhileLoop.cpp
+++ Day 005 - Loops/WhileLoop.cpp
@@ -1,1 +1,13 @@
+//Program using while loop.
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    while(n>0){
+        cout<<"n"<<endl;
+        cin>>n;
+    }
+    return 0;
+}
 
