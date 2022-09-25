64bd31a03747c1bfe2541ad0bb64702817646656
Kinjal Kanjiya <77874244+KinjalKanjiya@users.noreply.github.com>
2022-09-25 18:02:46 +0530

    Code added

--- Day 007 - Switch/Calculator.cpp
+++ Day 007 - Switch/Calculator.cpp
@@ -1,1 +1,37 @@
+// program to make a simple calculator.
+
+#include<iostream>
+using namespace std;
+int main()
+{
+    float a,b;
+    cout<<"Enter Two Numbers: ";
+    cin>>a>>b;
+    char op;
+    cout<<"Enter an operator:";
+    cin>>op;
+    switch (op)
+    {
+    case '+':
+        cout<<a+b<<endl;
+        break;
+    
+    case '-':
+        cout<<a-b<<endl;
+        break;
+    
+    case '*':
+        cout<<a*b<<endl;
+        break;
+    
+    case '/':
+        cout<<a/b<<endl;
+        break;
+    default:
+        cout<<"please enter valid operator";
+        break;
+    }
+    return 0;
+}
+
 
