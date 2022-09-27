f794785d8e431088c99ee544a2bffbf5cb5639dc
Kinjal Kanjiya <77874244+KinjalKanjiya@users.noreply.github.com>
2022-09-27 18:34:27 +0530

    Code added

---
+++ Day 009 - Pattern Questions/Half_Inverted_Payramid.cpp
@@ -0,0 +1,22 @@
+// Program to print following pattern.
+
+// *****
+// ****
+// ***
+// **
+// *
+
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cin>>n;
+    for(int i=n;i>=1;i--){
+        for(int j=1;j<=i;j++){
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
