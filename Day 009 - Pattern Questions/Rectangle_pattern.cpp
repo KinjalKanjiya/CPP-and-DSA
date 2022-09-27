acdf924067450105433dd3e48cfaadf0bc9ef07a
Kinjal Kanjiya <77874244+KinjalKanjiya@users.noreply.github.com>
2022-09-27 18:43:56 +0530

    Code added

--- Day 009 - Pattern Questions/Rectangle_pattern.cpp
+++ Day 009 - Pattern Questions/Rectangle_pattern.cpp
@@ -1,1 +1,29 @@
+// // Program to print following pattern.
+// ****
+// ****
+// ****
+// ****
+// ****
 
+
+
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int row,col;
+    cin>>row>>col;
+
+    for(int i=1;i<=row;i++)
+    {
+        for(int j=1;j<=col;j++)
+        {
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+    
+    return 0;
+}
