ea431817a71e526f7685e2d1e7e7f292d0067a6e
Kinjal Kanjiya <77874244+KinjalKanjiya@users.noreply.github.com>
2022-09-28 20:04:57 +0530

    Code added

--- Day 010 - Awesome Pattern Questions/Rhombus_Pattern.cpp
+++ Day 010 - Awesome Pattern Questions/Rhombus_Pattern.cpp
@@ -1,1 +1,32 @@
+// Program to print follwing pattern.
 
+//     * * * * * 
+//    * * * * *
+//   * * * * *
+//  * * * * *
+// * * * * *
+
+
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cin>>n;
+
+    for(int i=1;i<=5;i++)
+    {
+        for(int j=1;j<=n-i;j++)
+        {
+            cout<<" ";
+        }
+        for(int j=1;j<=n;j++)
+        {
+            cout<<"* ";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
+Footer
+© 2022 
