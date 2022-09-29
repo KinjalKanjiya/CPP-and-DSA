4b6c5bee5e09bec775cc267480b182b320687561
Kinjal Kanjiya <77874244+KinjalKanjiya@users.noreply.github.com>
2022-09-29 21:21:03 +0530

    Code added

--- Day 011-Prime-Composite using Sqrt function/Main.cpp
+++ Day 011-Prime-Composite using Sqrt function/Main.cpp
@@ -1,1 +1,24 @@
+// Program to check Prime-Composite using Sqrt function.
 
+
+#include<iostream>
+#include<cmath>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    bool flag=0;
+    for(int i=2;i<=sqrt(n);i++){
+        if(n%i==0){
+        cout<<"Non-Prime Number"<<endl;
+        flag=1;
+        break;
+        }
+    }
+
+if(flag==0){
+    cout<<"Prime-Number"<<endl;
+}
+return 0;
+
+}
