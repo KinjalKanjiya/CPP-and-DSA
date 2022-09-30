d19470967831467206140d3f24d9b56407a611a5
Kinjal Kanjiya <77874244+KinjalKanjiya@users.noreply.github.com>
2022-09-30 22:09:02 +0530

    Code added

--- Day 012 - Reverse of a given number n/Main.cpp
+++ Day 012 - Reverse of a given number n/Main.cpp
@@ -1,1 +1,22 @@
 
+// Program to find Reverse of a number.
+
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin>>n;
+    int lastdigit,reverse=0;
+
+    while(n!=0)
+    {
+        lastdigit=n%10;
+        reverse=reverse*10+lastdigit;
+        n=n/10;
+    }
+    cout<<reverse<<endl;
+    return 0;
+}
