391abd0369a06f850faf2a37cc4b5861c9c9da00
Kinjal Kanjiya <77874244+KinjalKanjiya@users.noreply.github.com>
2022-10-01 20:16:29 +0530

    Code added

--- Day 013 - Armstrong Or Not/main.cpp
+++ Day 013 - Armstrong Or Not/main.cpp
@@ -1,1 +1,25 @@
 
+//  Program Check if a given number is armstrong number or not.
+#include<iostream>
+#include<math.h>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    int sum=0;
+    int lastdigit;
+    int originaln=n;
+    while (n>0)
+    {
+        lastdigit = n%10;
+        sum += pow(lastdigit,3);
+        n=n/10;
+    }
+    if(sum==originaln){
+        cout<<"Armstrong Number"<<endl;
+    }
+    else{
+        cout<<"Not Armstrong Number"<<endl;
+    }
+    return 0;
+}
