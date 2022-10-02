3a5f30731336bfb757ea84472c8c62c3c4aa26ce
Kinjal Kanjiya <77874244+KinjalKanjiya@users.noreply.github.com>
2022-10-02 18:20:56 +0530

    Code added

--- Day 014 - Function/main.cpp
+++ Day 014 - Function/main.cpp
@@ -1,1 +1,19 @@
+// Function - Set of instructions to perform a perticular task.only names of variable are passed and not their types while calling a function. We pass the values, not variables themselves. Local variables are created in functions which are destroyed on returning from them.
 
+
+#include<iostream>
+using namespace std;
+
+int add(int num1,int num2)
+{
+    int sum= num1+num2;
+    return sum;
+}
+
+int main()
+{
+    int a,b;
+    cin>>a>>b;
+    cout<<add(a,b)<<endl;
+    return 0;
+}
