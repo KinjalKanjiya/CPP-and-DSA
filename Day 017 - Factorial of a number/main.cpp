c0f820caf816e778eb923ba730bd0d4399a919ce
Kinjal Kanjiya <77874244+KinjalKanjiya@users.noreply.github.com>
2022-10-05 20:47:29 +0530

    Update main.cpp

--- Day 017 - Factorial of a number/main.cpp
+++ Day 017 - Factorial of a number/main.cpp
@@ -1,1 +1,20 @@
+#include<iostream>
+using namespace std;
+int fact(int n){
+    int factorial=1;
+    for(int i=2;i<=n;i++){
+        factorial=factorial*i;
 
+    }
+    return factorial;
+}
+
+
+int main(){
+    int n;
+    cout<<"Enter a number: ";
+    cin>>n;
+    int ans = fact(n);
+    cout<<"Factorial is :";
+    cout<<ans<<endl;
+}
