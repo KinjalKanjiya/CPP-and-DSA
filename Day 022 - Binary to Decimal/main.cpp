d622547763eb23d82f915fa61977ba78b59614b2
Kinjal Kanjiya <77874244+KinjalKanjiya@users.noreply.github.com>
2022-10-10 22:01:26 +0530

    Update main.cpp

--- Day 022 - Binary to Decimal/main.cpp
+++ Day 022 - Binary to Decimal/main.cpp
@@ -1,1 +1,25 @@
 
+#include<iostream>
+using namespace std;
+
+int binaryToDecimal(int n)
+{
+    int ans=0;
+    int poweranswer=1;
+    while (n>0) // logic of conversion.
+    {
+       int lastdigit=n%10;
+       ans=ans+(lastdigit*poweranswer);
+       poweranswer=poweranswer*2;
+       n=n/10;
+    }
+    return ans;
+}
+
+int main()
+{
+    int n;
+    cin>>n;
+    cout<<binaryToDecimal(n)<<endl;
+    return 0;
+}
