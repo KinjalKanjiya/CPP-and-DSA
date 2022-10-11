441ba916bc135f4b072b9c4266805e67d7f40b40
Kinjal Kanjiya <77874244+KinjalKanjiya@users.noreply.github.com>
2022-10-11 20:13:57 +0530

    Code added

--- Day 023 - Octal To Decimal/main.cpp
+++ Day 023 - Octal To Decimal/main.cpp
@@ -1,3 +1,28 @@
 // Program to convert octal number to decimal number.
+#include<iostream>
+using namespace std;
 
+int octalToDecimal(int n)
+{
+    int ans=0;
+    int poweranswer=1;
+
+    while(n>0) // conversion logic
+    {
+        int lastdigit=n%10;
+        ans=ans+lastdigit*poweranswer;
+        poweranswer=poweranswer*8;
+        n=n/10;
+    }
+    return ans;
+}
+
+int main()
+{
+    int n;
+    cin>>n;
+
+    cout<<octalToDecimal(n)<<endl;
+    return 0;
+}
 
