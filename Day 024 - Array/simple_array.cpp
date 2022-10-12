a076405c363f1cf01b8ba4a217f27aabc12faef7
Kinjal Kanjiya <77874244+KinjalKanjiya@users.noreply.github.com>
2022-10-12 20:32:29 +0530

    Code added

--- Day 024 - Array/simple_array.cpp
+++ Day 024 - Array/simple_array.cpp
@@ -1,2 +1,10 @@
 // Array : group of values of same data type.
+#include<iostream>
+using namespace std;
+int main(){
+    
+  // values at the time of initialization
+    int array[4]={1,2,3,4};
+    cout<<array[3]<<endl;
 
+}
