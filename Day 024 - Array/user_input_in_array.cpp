6bbb5a415838e950e39445feaadd972140900cf8
Kinjal Kanjiya <77874244+KinjalKanjiya@users.noreply.github.com>
2022-10-12 20:34:55 +0530

    Update user_input_in_array.cpp

--- Day 024 - Array/user_input_in_array.cpp
+++ Day 024 - Array/user_input_in_array.cpp
@@ -1,1 +1,20 @@
+// Program to Take a value from the user in array and print it.
+#include<iostream>
+using namespace std;
 
+int main(){
+
+    int n;
+    cin>>n;
+
+    int array[n];
+    for(int i=0;i<n;i++){
+        cin>>array[i];
+        //cout<<endl;
+    }
+    for(int i=0;i<n;i++){
+        cout<<array[i]<<" ";
+    }
+    return 0;
+
+}
