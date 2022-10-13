0f299c36c47a3f28cafa0634233b3cb04e1f0f7a
Kinjal Kanjiya <77874244+KinjalKanjiya@users.noreply.github.com>
2022-10-13 21:34:10 +0530

    Code added

--- Day 025 - Min Max num from Array/main.cpp
+++ Day 025 - Min Max num from Array/main.cpp
@@ -1,4 +1,25 @@
 // Program to find Minimum and Maximum number from Array.
 
+#include<iostream>
+#include<climits>
+using namespace std;
+int main(){
+     
+     int n;
+     cin>>n;
+     int array[n];
+     for(int i=0;i<n;i++){
+         cin>>array[i];
+     }
+     int maxno=INT_MIN;
+     int minno=INT_MAX;
+     for(int i=0;i<n;i++){
+         maxno=max(maxno,array[i]);
+         minno=min(minno,array[i]);
 
+     }
+     cout<<"maximum number : "<<maxno<<" Minimum number: "<<minno<<endl;
+     return 0;
+
+}
 
