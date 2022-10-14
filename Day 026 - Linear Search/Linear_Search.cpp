e9404014c7c443bd1000da7ae35d518e076ba3b9
Kinjal Kanjiya <77874244+KinjalKanjiya@users.noreply.github.com>
2022-10-14 22:15:36 +0530

    Code added

--- Day 026 - Linear Search/Linear_Search.cpp
+++ Day 026 - Linear Search/Linear_Search.cpp
@@ -1,5 +1,36 @@
 // Program to search element in an array in one by one.
 
 
+#include<iostream>
+using namespace std;
+//Time Complaxity is O(n)
+//To Search Element in array
+int LinearSearch(int arr[], int n, int key){
+    for(int i =0;i<n;i++){
+        if(arr[i]==key){
+            return i;
+        }
+    }
+    return -1;
+}
+
+int main(){
+    //Size of array
+    int n;
+    cout<<"Enter Size of Array: "<<endl;
+    cin>>n;
+    cout<<"Enter Element of an Array"<<endl;
+    int arr[n];
+    //To Array UserInput
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    cout<<"Enter Search Element in Array:"<<endl;
+    int key;
+    cin>>key;
+    cout<<LinearSearch(arr,n,key);
+    return 0;
+}
+
 
 
