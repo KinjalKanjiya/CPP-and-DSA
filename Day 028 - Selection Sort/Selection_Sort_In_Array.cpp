ec4787b190f4c55bbfb7337b250b65a6954dba9b
Kinjal Kanjiya <77874244+KinjalKanjiya@users.noreply.github.com>
2022-10-16 21:10:36 +0530

    Code added

--- Day 028 - Selection Sort/Selection_Sort_In_Array.cpp
+++ Day 028 - Selection Sort/Selection_Sort_In_Array.cpp
@@ -1,1 +1,33 @@
+// Selection Sort- Find the minimum array in unsorted array and swap it with element at beginning.
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"Enter size of an array:";
+    cin>>n;
+    int arr[n];
+    cout<<"Enter Elements of an Array:";
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    // selection Sort Logic
+    for(int i=0;i<n-1;i++){
+        for(int j=i+1;j<n;j++){
+            if(arr[j]<arr[i]){
+                int temp;
+                temp = arr[j];
+                arr[j]= arr[i];
+                arr[i]= temp;
+            }
+        }
 
+    }
+
+    cout<<"Sorted Array is :";
+    for(int i=0;i<n;i++){
+        cout<<arr[i]<<" ";
+        
+    }
+    return 0;
+
+}
