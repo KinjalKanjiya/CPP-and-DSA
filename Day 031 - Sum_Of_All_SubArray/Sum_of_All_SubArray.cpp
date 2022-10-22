ca88ba2d76063e802be2ed8de7ca6d86e09b8afd
Kinjal Kanjiya <77874244+KinjalKanjiya@users.noreply.github.com>
2022-10-22 21:48:53 +0530

    Update Sum_of_All_SubArray.cpp

--- Day 031 - Sum_Of_All_SubArray/Sum_of_All_SubArray.cpp
+++ Day 031 - Sum_Of_All_SubArray/Sum_of_All_SubArray.cpp
@@ -1,1 +1,23 @@
+//Problem: Given an array a[] of size n. Output sum of each subarray of the given array.
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"Enter Size of an Array:";
+    cin>>n;
+    int arr[n];
+    cout<<"Enter Number of an elements in Array: ";
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    cout<<"Sum of SubArray is :";
+    int sum =0;
+    for(int i=0;i<n;i++){
+        sum = 0;
+        for(int j=i;j<n;j++){
+            sum+=arr[j];
+            cout<<sum<<endl;
 
+        }
+    }
+}
