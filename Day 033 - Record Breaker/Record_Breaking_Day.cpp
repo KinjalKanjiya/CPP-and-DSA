c28fd8bf3c22b5064ae8c8398510b290d333276e
Kinjal Kanjiya <77874244+KinjalKanjiya@users.noreply.github.com>
2022-10-25 22:03:23 +0530

    Code added

--- Day 033 - Record Breaker/Record_Breaking_Day.cpp
+++ Day 033 - Record Breaker/Record_Breaking_Day.cpp
@@ -1,1 +1,30 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"Enter Size od an Array: ";
+    cin>>n;
+    int arr[n+1];
+    arr[n]=-1;
+    cout<<"Enter a Elements of an Array: ";
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
 
+    if(n==1){
+        cout<<"1"<<endl;
+        return 0;
+    }
+
+    int ans = 0;
+    int mx = -1;
+    for(int i=0;i<n;i++){
+    if(arr[i]>mx && arr[i]>arr[i+1]){
+        ans++;
+    }
+        mx = max(mx, arr[i]);
+    }
+
+    cout<<"Record Breaking Day is:"<<ans<<endl;
+
+}
