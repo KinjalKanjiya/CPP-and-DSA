5ce763eaf5952b57f93db2b9415c5d4f5daa81d2
Kinjal Kanjiya <77874244+KinjalKanjiya@users.noreply.github.com>
2022-10-27 20:51:33 +0530

    Update Main.cpp

--- Day 034 - First_Repeating_Element/Main.cpp
+++ Day 034 - First_Repeating_Element/Main.cpp
@@ -12,5 +12,36 @@ int main()
     {
         cin>>a[i];
     }
-  
+   const int size=1e6+2; // taking care of constraints
+    int index[size];
+
+    for(int i=0;i<size;i++)
+    {
+        index[i]=-1;
+    }
+
+    int minindex = INT_MAX;
+    for(int i=0;i<n;i++)
+    {
+        if(index[a[i]]!=-1)
+        {
+            minindex=min(minindex,index[a[i]]);
+        }
+        else
+        {
+            index[a[i]]=i;
+        }
+    }
+
+    if(minindex == INT_MAX)
+    {
+        cout<<"-1";
+    }
+    else
+    {
+        cout<<minindex;
+    }
+
+    return 0;
+}
 
