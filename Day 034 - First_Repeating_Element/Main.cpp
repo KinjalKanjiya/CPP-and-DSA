a1b60796725f10243efb28d5cb3eb98eb086ca9d
Kinjal Kanjiya <77874244+KinjalKanjiya@users.noreply.github.com>
2022-10-27 20:50:52 +0530

    Update Main.cpp

--- Day 034 - First_Repeating_Element/Main.cpp
+++ Day 034 - First_Repeating_Element/Main.cpp
@@ -1,1 +1,16 @@
+#include<iostream>
+#include<climits>
+using namespace std;
+
+int main()
+{
+    int n;
+    cout<<"Enter Number of elements: ";
+    cin>>n;
+    int a[n];
+    for(int i=0;i<n;i++) // user input
+    {
+        cin>>a[i];
+    }
+  
 
