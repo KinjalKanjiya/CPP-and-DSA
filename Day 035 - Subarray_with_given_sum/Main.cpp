411dae3ea34a6780df21a5f33d1f542475043b05
Kinjal Kanjiya <77874244+KinjalKanjiya@users.noreply.github.com>
2022-10-28 22:37:25 +0530

    Code added

--- Day 035 - Subarray_with_given_sum/Main.cpp
+++ Day 035 - Subarray_with_given_sum/Main.cpp
@@ -10,3 +10,30 @@ int main()
     {
         cin>>a[i];
     }
+    int curr=0,st=0,en=-1,j=0;
+    xyz:
+        //cout<<"pass: "<<st<<endl;
+        while(j<n && curr<=sum)
+        {
+            curr=curr+a[j];
+            if(curr==sum)
+            {
+                en=j;
+                cout<<"Starting index:"<<st<<endl<<"Ending index:"<<en<<endl;
+                return 0;
+            }
+            j++;
+        }
+
+    //cout<<"current sum after pass-"<<st<<":"<<curr<<endl;
+    if(curr!=sum && st<n)
+    {
+        st++;
+        j=st;
+        en=st;
+        curr=0;
+        goto xyz;
+    }
+    cout<<"-1";
+    return 0;
+}
