f245ca5dd9b7a8d261ec4b1d2a8646108be31ba3
Kinjal Kanjiya <77874244+KinjalKanjiya@users.noreply.github.com>
2022-10-29 22:12:46 +0530

    Code added

--- Day 036 - Smallest Positive Missing Number/Main.cpp
+++ Day 036 - Smallest Positive Missing Number/Main.cpp
@@ -11,4 +11,30 @@ int main()
     {
         cin>>a[i];
     }
+     int N = 1e6+2; // due to constraint. 
+    bool check[N];
+    for(int i=0;i<N;i++)
+    {
+        check[i]=false;
+    }
+
+    for(int i=0;i<n;i++)
+    {
+        if(a[i]>=0)
+        {
+            check[a[i]]=true;
+        }
+    }
+
+    int ans=-1;
+    for(int i=1;i<N;i++)
+    {
+        if(check[i]==false)
+        {
+            ans=i;
+            break;
+        }
+    }
+    cout<<ans<<endl;
+    return 0;
 }
