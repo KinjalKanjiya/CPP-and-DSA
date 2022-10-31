63858e93a203b97334ad1ab9ee20665df6c6dc56
Kinjal Kanjiya <77874244+KinjalKanjiya@users.noreply.github.com>
2022-10-31 21:27:52 +0530

    Source code

--- Day 037 - All the Subarray of an Array/main.cpp
+++ Day 037 - All the Subarray of an Array/main.cpp
@@ -1,1 +1,27 @@
 
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin>>n;
+    int a[n];
+    for(int i=0;i<n;i++) // user input
+    {
+        cin>>a[i];
+    }
+
+    for(int start=0;start<n;start++) // loop to select start point of a subarray
+    {
+        for(int end=start;end<n;end++) // loop to select end point of a subarray
+        {
+            for(int k=start;k<=end;k++) // loop to print the subarray
+            {
+                cout<<a[k]<<" ";
+            }
+            cout<<endl;
+        }
+    }
+    return 0;
+}
