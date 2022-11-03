0754bfa4c108af6b0713afb3adc3d64c42bc962b
Kinjal Kanjiya <77874244+KinjalKanjiya@users.noreply.github.com>
2022-11-03 21:01:54 +0530

    Code added

--- Day 037 - Maximum subarray Sum/Main.cpp
+++ Day 037 - Maximum subarray Sum/Main.cpp
@@ -1,1 +1,29 @@
+// Time Complexity = 0(n^2)
 
+#include<iostream>
+#include<climits>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin>>n;
+    int a[n];
+    for(int i=0;i<n;i++)
+    {
+        cin>>a[i];
+    }
+
+    int sum,maxSum=INT_MIN;
+    for(int i=0;i<n;i++) //starting point selection
+    {
+        for(int j=i;j<n;j++)  // sum of individual subarrays.
+        {
+            sum+=a[j];
+            maxSum=max(sum,maxSum);
+        }
+        sum=0;
+    }
+    cout<<maxSum<<endl;
+    return 0;
+}
