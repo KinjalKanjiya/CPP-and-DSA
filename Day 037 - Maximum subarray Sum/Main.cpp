4be66de088ce639d0c11b6d515aacc28fe6ae9aa
Kinjal Kanjiya <77874244+KinjalKanjiya@users.noreply.github.com>
2022-11-03 21:02:57 +0530

    Coed added

--- Day 037 - Maximum subarray Sum/Main.cpp
+++ Day 037 - Maximum subarray Sum/Main.cpp
@@ -1,4 +1,5 @@
-// Time Complexity = 0(n^2)
+// Find the sum of a subarray which is maximum of all the sums subarrays.
+// Time Complexity = 0(n^3)
 
 #include<iostream>
 #include<climits>
@@ -13,16 +14,53 @@ int main()
     {
         cin>>a[i];
     }
-
-    int sum,maxSum=INT_MIN;
-    for(int i=0;i<n;i++) //starting point selection
+    
+    int maxSum=INT_MIN;
+    int sum=0;
+    for(int i=0;i<n;i++)// to select starting point
     {
-        for(int j=i;j<n;j++)  // sum of individual subarrays.
+        for(int j=0;j<n;j++) // to select ending point
         {
-            sum+=a[j];
-            maxSum=max(sum,maxSum);
+            for(int k=i;k<=j;k++) // to find sum of an subarray.
+            {
+               sum+=a[k]; 
+            }
+            maxSum=max(maxSum,sum);
+            sum=0;
         }
-        sum=0;
+    }
+    cout<<maxSum<<endl;
+    return 0;
+}
+
+
+#include<iostream>
+#include<climits>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin>>n;
+    int a[n];
+    for(int i=0;i<n;i++)
+    {
+        cin>>a[i];
+    }
+    
+    int maxSum=INT_MIN;
+    int sum=0;
+    for(int i=0;i<n;i++)// to select starting point
+    {
+        for(int j=0;j<n;j++) // to select ending point
+        {
+            for(int k=i;k<=j;k++) // to find sum of an subarray.
+            {
+               sum+=a[k]; 
+            }
+            maxSum=max(maxSum,sum);
+            sum=0;
+        }
     }
     cout<<maxSum<<endl;
     return 0;
