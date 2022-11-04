4ae12f62978e4fe5d2fb36fb4f68c3ae9471a260
Kinjal Kanjiya <77874244+KinjalKanjiya@users.noreply.github.com>
2022-11-04 20:41:44 +0530

    Update main.cpp

--- Day 039 - Kadane's Algorithm/main.cpp
+++ Day 039 - Kadane's Algorithm/main.cpp
@@ -1,1 +1,28 @@
+#include<iostream>
+#include<climits>
+using namespace std;
 
+int main()
+{
+    int n;
+    cin>>n;
+    int a[n];
+    for(int i=0;i<n;i++) //user input
+    {
+        cin>>a[i];
+    }
+
+    int maxSum=INT_MIN;
+    int currentSum=0;
+    for(int i=0;i<n;i++) // kadanes algo
+    {
+        currentSum=a[i];
+        maxSum=max(maxSum,currentSum);
+        if(currentSum<0)
+        {
+            currentSum=0;
+        }
+    }
+    cout<<maxSum<<endl;
+    return 0;
+}
