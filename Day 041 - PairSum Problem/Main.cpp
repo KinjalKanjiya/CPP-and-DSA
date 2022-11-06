7cd22b7b05cb21148b5b152163bb334f58d35a2d
Kinjal Kanjiya <77874244+KinjalKanjiya@users.noreply.github.com>
2022-11-06 22:03:01 +0530

    Update Main.cpp

--- Day 041 - PairSum Problem/Main.cpp
+++ Day 041 - PairSum Problem/Main.cpp
@@ -1,1 +1,19 @@
+#include<iostream>
+using namespace std;
+
+bool pairSum(int a[],int n,int k)
+{
+    for(int i=0;i<n-1;i++)
+    {
+        for(int j=i+1;j<n;j++)
+        {
+            if(a[j]+a[i]==k)
+            {
+                cout<<i<<" "<<j<<endl; // printing indices of the elements.
+                return true;
+            }
+        }
+    }
+    return false;
+}
 
