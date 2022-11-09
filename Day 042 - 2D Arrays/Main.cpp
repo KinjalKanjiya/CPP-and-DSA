57a514801b018bfac4689189b81505c90e7c0a02
Kinjal Kanjiya <77874244+KinjalKanjiya@users.noreply.github.com>
2022-11-09 22:40:49 +0530

    Update Main.cpp

--- Day 042 - 2D Arrays/Main.cpp
+++ Day 042 - 2D Arrays/Main.cpp
@@ -1,1 +1,15 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n,m;
+    cout<<"Enter number of Rows:";
+    cin>>n;
+    cout<<"Enter number of Columns:";
+    cin>>m;
+    int a[n][m];
+    for(int i=0;i<n;i++){
+        for(int j=0;j<m;j++){
+            cin>>a[i][j];
+        }
+    }
 
