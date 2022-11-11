00237038efa7b92a7889403b97b8cb0b8811d441
Kinjal Kanjiya <77874244+KinjalKanjiya@users.noreply.github.com>
2022-11-11 23:27:23 +0530

    Code added

--- Day 043 - Search element in matrix/main.cpp
+++ Day 043 - Search element in matrix/main.cpp
@@ -12,3 +12,33 @@ int main(){
             cin>>a[i][j];
         }
     }
+
+    cout<<"Matrix is :"<<endl;
+    for(int i=0;i<n;i++){
+        for(int j=0;j<m;j++){
+            cout<<a[i][j]<<" ";
+        }
+        cout<<endl;
+
+    }
+    int x;
+    cout<<"Element is :";
+    cin>>x;
+    bool flag=false;
+    
+    for(int i=0;i<n;i++){
+        for(int j=0;j<m;j++){
+            if(a[i][j]==x){
+                cout<<"Element is in Row "<<i<<" Column "<<j<<endl;
+                flag=true;
+            }
+        }
+    }
+    if(flag){
+        cout<<"Element is found\n";
+    }
+    else{
+        cout<<"Element is not found\n";
+    }
+
+}
