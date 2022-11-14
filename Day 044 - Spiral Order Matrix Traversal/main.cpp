c27b52130d929e306b1b8942c924fb74aa1c9547
Kinjal Kanjiya <77874244+KinjalKanjiya@users.noreply.github.com>
2022-11-14 23:03:24 +0530

    Update main.cpp

--- Day 044 - Spiral Order Matrix Traversal/main.cpp
+++ Day 044 - Spiral Order Matrix Traversal/main.cpp
@@ -1,1 +1,16 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n,m;
+    cout<<"Enter Number of Rows: ";
+    cin>>n;
+    cout<<"Enter Number of Columns: ";
+    cin>>m;
+    int a[n][m];
+    cout<<"Enter the Element of an Array: "<<endl;
+    for(int i=0;i<n;i++){
+        for(int j=0;j<m;j++){
+            cin>>a[i][j];
+        }
+    }
 
