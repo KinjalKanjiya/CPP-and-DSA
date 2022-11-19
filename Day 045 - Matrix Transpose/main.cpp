35868e9d0eddf8eb5d16c433327fded65d416e9d
Kinjal Kanjiya <77874244+KinjalKanjiya@users.noreply.github.com>
2022-11-19 20:56:55 +0530

    Code added

--- Day 045 - Matrix Transpose/main.cpp
+++ Day 045 - Matrix Transpose/main.cpp
@@ -21,4 +21,12 @@ int main()
             a[j][i]=temp;
         }
     }
-  
+  //Print Matrix
+    for(int i=0;i<n;i++){
+        for(int j=0;j<m;j++){
+            cout<<a[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
